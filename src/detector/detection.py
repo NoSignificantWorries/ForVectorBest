7da4f01a4c0657c4587707afa853d8e422d7e287
M1roQ <b_val4@mail.ru>
2025-05-16 16:27:13 +0000

    update predict in detection.py && add script for crops

--- src/detector/detection.py
+++ src/detector/detection.py
@@ -48,7 +48,7 @@ class Detector:
     #         image_path = output_path / f"test_result_{i}.jpg"
     #         cv2.imwrite(str(image_path), im_bgr)
 
-    def predict(self, source: str, save_dir: str, params: dict = conf.DETECTOR_PARAMS):
+    def predict(self, source: str, save_dir: str, params: dict = conf.DETECTOR_PARAMS, allowed_classes: list = None):
         results = self.model.predict(source, imgsz=params["imgsz"], conf=params["conf"], iou=params["iou"])
 
         output_path = Path(save_dir)
@@ -63,16 +63,28 @@ class Detector:
 
             image_results = {
                 "image_path": r.path,
-                "text_boxes": []
+                "text_boxes": [],
+                "object_boxes": []
             }
 
             for box in r.boxes.data.cpu().numpy():
                 x1, y1, x2, y2, conf_score, class_id = box[0], box[1], box[2], box[3], box[4], int(box[5])
-                if class_id == 1:  # только текст
-                    image_results["text_boxes"].append({
-                        "bbox": [int(x1), int(y1), int(x2), int(y2)],
-                        "conf": float(conf_score)
-                    })
+                if allowed_classes is not None and class_id not in allowed_classes:
+                    continue
+
+                box_info = {
+                    "bbox": [int(x1), int(y1), int(x2), int(y2)],
+                    "conf": float(conf_score),
+                    "class": class_id
+                }
+
+                if class_id == 1:
+                    image_results["text_boxes"].append(box_info)
+                elif class_id == 0:
+                    image_results["object_boxes"].append(box_info)
+                else:
+                    # Если появятся другие классы, можно добавить сюда обработку
+                    pass
 
             predictions.append(image_results)
 
