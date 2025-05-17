7854d27b8017d0d6f7247cc4b18352ba16e4b6f3
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-05-17 16:05:25 +0000

    random forests was trained

--- utils/forest_train.py
+++ utils/forest_train.py
@@ -1,12 +1,14 @@
 import os
+import joblib
+import re
 
 import cv2
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import joblib 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from tqdm import tqdm
 
 import src.conf as conf
 
@@ -35,41 +37,148 @@ def main(csv_path: str, num_classes: int = conf.NUM_CLUSSES) -> None:
     df["width/height"] = df["crop_width"] / df["crop_height"]
     df["height/width"] = df["crop_height"] / df["crop_width"]
     
-    image_params = {}
-    for i in range(len(df)):
-        path = os,path.join("local_data", "output", "Bad", df["crop_name"][i])
-        if not os.path.exists(path):
-            continue
-        path = os,path.join("local_data", "output", "Good", df["crop_name"][i])
-        if not os.path.exists(path):
-            continue
-        img = cv2
-
-    df = df.drop(["image_name", "crop_name", "image_width", "image_height"], axis=1)
-    
-    X = df.drop(["class"], axis=1)
+    X = df.drop(["class", "image_name", "crop_name", "image_width", "image_height"], axis=1)
     print(X.columns)
     y = df["class"]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = RandomForestClassifier(n_estimators=50, random_state=42)
-    model.fit(X_train, y_train)
+    model = joblib.load(conf.BBOX_CLASSIFIER_PATH)
+    # model = RandomForestClassifier(n_estimators=50, random_state=42)
+    # model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
     print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-score: {f1}")
+    
+    print("===============")
+    y_pred = model.predict(X)
+
+    accuracy = accuracy_score(y, y_pred)
+    precision = precision_score(y, y_pred, average='weighted')
+    recall = recall_score(y, y_pred, average='weighted')
+    f1 = f1_score(y, y_pred, average='weighted')
+    print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-score: {f1}")
     
     feature_importances = model.feature_importances_
 
-    feature_importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
-    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+    feature_importance_df = pd.DataFrame({"Feature": X.columns, "Importance": feature_importances})
+    feature_importance_df = feature_importance_df.sort_values(by="Importance", ascending=False)
 
     print("Importances:")
     print(feature_importance_df)
 
-    joblib.dump(model, "forest_model.pkl")
+    joblib.dump(model, "tmp/boxes_classifier.pkl")
+    print("Model saved successfully.")
+    
+
+    '''
+    cols_from_prev_df = ["x", "y", "x1", "x2", "y1", "y2", "area", "height/width", "width/height", "crop_height", "crop_height_%", "crop_width", "crop_width_%"]
+    df2 = {"name": []}
+    for i in range(conf.NUM_CLUSSES):
+        df2[f"class_{i}"] = []
+        for elem in cols_from_prev_df:
+            df2[f"{elem}_{i}"] = []
+        for j in range(conf.NUM_CLUSSES):
+            if i == j:
+                continue
+            df2[f"{i}_to_{j}_x"] = []
+            df2[f"{i}_to_{j}_y"] = []
+    df2["good/bad"] = []
+
+    for image_name in df["image_name"].unique():
+        local_df = df[df["image_name"] == image_name]
+
+        path = os.path.join("local_data", "output", "Bad", image_name)
+        df2["name"].append(image_name)
+        if os.path.exists(path):
+            df2["good/bad"].append(0)
+        else:
+            df2["good/bad"].append(1)
+        for i in range(conf.NUM_CLUSSES):
+            if local_df[local_df["class"] == i].shape[0] == 0:
+                df2[f"class_{i}"].append(0)
+                for elem in cols_from_prev_df:
+                    df2[f"{elem}_{i}"].append(0)
+            else:
+                df2[f"class_{i}"].append(1)
+                for elem in cols_from_prev_df:
+                    df2[f"{elem}_{i}"].append(float(local_df[local_df["class"] == i][elem].iloc[0]))
+            for j in range(conf.NUM_CLUSSES):
+                if i == j:
+                    continue
+                if local_df[local_df["class"] == i].shape[0] == 0 or local_df[local_df["class"] == j].shape[0] == 0:
+                    df2[f"{i}_to_{j}_x"].append(0)
+                    df2[f"{i}_to_{j}_y"].append(0)
+                else:
+                    vec_x = float(local_df[local_df["class"] == j]["x"].iloc[0]) - float(local_df[local_df["class"] == i]["x"].iloc[0])
+                    vec_y = float(local_df[local_df["class"] == j]["y"].iloc[0]) - float(local_df[local_df["class"] == i]["y"].iloc[0])
+                    df2[f"{i}_to_{j}_x"].append(vec_x)
+                    df2[f"{i}_to_{j}_y"].append(vec_y)
+
+            
+    df2 = pd.DataFrame(df2)
+    df2.to_csv("tmp/tmp.csv")
+    '''
+    
+    df2 = pd.read_csv("tmp/tmp.csv")
+    
+    print(len(df2.columns))
+
+    X = df2.drop(["name", "good/bad"], axis=1)
+    print(X.columns)
+    y = df2["good/bad"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = joblib.load(conf.PATTERN_CLASSIFIER_PATH)
+    # model = RandomForestClassifier(n_estimators=50, random_state=42)
+    # model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-score: {f1}")
+    
+    print("===============")
+    y_pred = model.predict(X)
+
+    accuracy = accuracy_score(y, y_pred)
+    precision = precision_score(y, y_pred, average='weighted')
+    recall = recall_score(y, y_pred, average='weighted')
+    f1 = f1_score(y, y_pred, average='weighted')
+    print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-score: {f1}")
+    
+    feature_importances = model.feature_importances_
+
+    feature_importance_df2 = pd.DataFrame({"Feature": X.columns, "Importance": feature_importances})
+    feature_importance_df2 = feature_importance_df2.sort_values(by="Importance", ascending=False)
+
+    print("Importances:")
+    print(feature_importance_df2)
+    
+    print(feature_importance_df2[feature_importance_df2["Importance"] > 0.0])
+
+    joblib.dump(model, "tmp/pattern_classifier.pkl")
     print("Model saved successfully.")
 
 
